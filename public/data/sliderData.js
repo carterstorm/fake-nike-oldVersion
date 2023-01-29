80dbc59af86c7da025821c513808e4bd58178ad5
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-01-29 15:54:46 +0100

    created sliderData file and added slider img

--- public/data/sliderData.js
+++
@@ -1,8 +0,0 @@
-import FitnessGirl from "../images/fitness-girl-jogging.jpg"
-
-export const sliderData = [
-    {
-        id: 1,
-        image: FitnessGirl,
-    },
-];
---
+++ src/data/sliderData.js
@@ -0,0 +1,13 @@
+import FitnessGirl from "../images/fitness-girl-jogging.jpg";
+import Cyclist from "../images/cyclist.jpg";
+
+export const sliderData = [
+    {
+        id: 1,
+        image: FitnessGirl,
+    },
+    {
+        id: 2,
+        image: Cyclist,
+    }
+];
