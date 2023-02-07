51f81f854221c7a044eaf4fe918edd7286847b5a
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-02-07 23:14:36 +0100

    changed styled Arrow from div to button element

--- src/common/Arrow/styled.js
+++ src/common/Arrow/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledArrow = styled.div`
+export const StyledArrow = styled.button`
     position: absolute;
     z-index: 2;
     margin: auto;
