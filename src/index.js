492ae64f5ec48be8053a2d69b88cb254d20ca128
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-02-12 00:36:25 +0100

    imported ThemeProvider and created theme file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './GlobalStyle';
 import { App } from './App';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
