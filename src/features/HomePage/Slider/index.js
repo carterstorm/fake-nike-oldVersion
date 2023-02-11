a63a86cbacf5a0e967d6860876937cf26200489c
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-02-11 13:34:53 +0100

    changed file name to useData.js

--- src/features/HomePage/Slider/index.js
+++ src/features/HomePage/Slider/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Arrow } from "../../../common/Arrow";
-import { useData } from "../../../useSliderData";
+import { useData } from "../../../useData";
 
 import {
     Container,
