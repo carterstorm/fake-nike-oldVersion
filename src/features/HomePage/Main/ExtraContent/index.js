34fae01209c0ca917f5c96fe506bbd1ff9f2ce7f
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-02-13 22:35:01 +0100

    styled ExtraContent component

--- src/features/HomePage/Main/ExtraContent/index.js
+++ src/features/HomePage/Main/ExtraContent/index.js
@@ -2,13 +2,13 @@ import {
     Container,
     Heading,
     Image,
+    ImageContainer,
     Item,
     Link,
-    LinkContainer,
     LinksContainer,
     List,
     MenuItem,
-    Stylization
+    Stylization,
 } from "./styled";
 
 export const ExtraContent = () => {
@@ -18,13 +18,37 @@ export const ExtraContent = () => {
             <List>
                 <Item>
                     <Link>
-                        <LinkContainer>
-                            <Image />
-                            <LinksContainer>
-                                <MenuItem></MenuItem>
-                                <Stylization></Stylization>
-                            </LinksContainer>
-                        </LinkContainer>
+                        <ImageContainer>
+                            <Image src="https://images.pexels.com/photos/2468344/pexels-photo-2468344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                        </ImageContainer>
+                        <LinksContainer>
+                            <MenuItem>Kobiety</MenuItem>
+                            <Stylization>Pokaż stylizację</Stylization>
+                        </LinksContainer>
+                    </Link>
+                </Item>
+
+                <Item>
+                    <Link>
+                        <ImageContainer>
+                            <Image src="https://images.pexels.com/photos/2468344/pexels-photo-2468344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                        </ImageContainer>
+                        <LinksContainer>
+                            <MenuItem>Kobiety</MenuItem>
+                            <Stylization>Pokaż stylizację</Stylization>
+                        </LinksContainer>
+                    </Link>
+                </Item>
+
+                <Item>
+                    <Link>
+                        <ImageContainer>
+                            <Image src="https://images.pexels.com/photos/2468344/pexels-photo-2468344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                        </ImageContainer>
+                        <LinksContainer>
+                            <MenuItem>Kobiety</MenuItem>
+                            <Stylization>Pokaż stylizację</Stylization>
+                        </LinksContainer>
                     </Link>
                 </Item>
             </List>
--- src/features/HomePage/Main/ExtraContent/styled.js
+++ src/features/HomePage/Main/ExtraContent/styled.js
@@ -12,27 +12,65 @@ export const Heading = styled.h3`
 `;
 
 export const List = styled.ul`
-
+    list-style: none;
+    padding: 0;
+    display: flex;
+    gap: 20px;
 `;
 
 export const Item = styled.li`
-
+    flex-grow: 1;
 `;
 
 export const Link = styled.a`
+    position: relative;
+    top: 0;
+    left: 0;
+
+    &:hover {
+        cursor: pointer;
+    }
 `;
 
-export const LinkContainer = styled.div`
+export const ImageContainer = styled.div`
+
 `;
 
 export const Image = styled.img`
+    width: 100%;
+    height: 35vw;
+    object-fit: cover;
 `;
+
 
 export const LinksContainer = styled.div`
+    position: absolute;
+    bottom: 60px;
+    left: 0;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
 `;
 
-export const MenuItem = styled.a`
+export const MenuItem = styled.span`
+    background-color: #fff;
+    padding: 10px 30px;
+    border-radius: 20px;
+    margin: 5px 10px;
+
+    &:hover {
+        background-color: ${({ theme }) => theme.colors.lightGray};
+    }
 `;
 
-export const Stylization = styled.a`
+export const Stylization = styled.span`
+    background-color: #fff;
+    padding: 10px 30px;
+    border-radius: 20px;
+    margin: 5px 10px;
+
+    &:hover {
+        background-color: ${({ theme }) => theme.colors.lightGray};
+    }
 `;
