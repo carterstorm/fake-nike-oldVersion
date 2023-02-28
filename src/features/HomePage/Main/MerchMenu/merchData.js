274791cd5d725e5fc29324517b81a5d286e27035
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-02-28 22:16:11 +0100

    created merchData file

---
+++ src/features/HomePage/Main/MerchMenu/merchData.js
@@ -0,0 +1,53 @@
+export const merchData = [
+    {
+        id: 1,
+        name: "Buty",
+        elements: [
+            "Buty do golfa",
+            "Zimowe buty sportowe",
+            "Buty sportowe z Gore Texu",
+            "Buty sportowe do chodzenia",
+            "Air Max 90 białe",
+            "Buty do podnoszenia cięzarów",
+        ],
+    },
+
+    {
+        id: 2,
+        name: "Ubrania",
+        elements: [
+            "Wszystkie ubrania",
+            "Spodnie do jogi",
+            "Joggery",
+            "Spodenie",
+            "Bluza z kapturem",
+            "Dresy",
+        ],
+    },
+
+    {
+        id: 3,
+        name: "Produkty dla dzieci",
+        elements: [
+            "Klapki dziecięce",
+            "Dresy dziecięce",
+            "Kurtki",
+        ],
+    },
+
+    {
+        id: 4,
+        name: "Polecane",
+        elements: [
+            "Kluby piłkarskie",
+            "Piłka nożna",
+            "Nike Run Club",
+            "Nike Training Club",
+            "Sandały i klapki",
+            "Buty do biegania, czarne",
+            "Buty do biegania, białe",
+            "Buty do piłki nożnej, niebieskie",
+            "Buty do piłki nożnej, czerwone",
+        ],
+    }
+];
