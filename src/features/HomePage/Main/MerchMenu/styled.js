fd5eb128951a95f97228bf6b98e76f4a1cb93d9c
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-02-28 22:19:04 +0100

    added styles and variables to styled components

--- src/features/HomePage/Main/MerchMenu/styled.js
+++ src/features/HomePage/Main/MerchMenu/styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.section`
     display: grid;
@@ -13,24 +13,41 @@ export const Category = styled.div`
 `;
 
 export const Heading = styled.h4`
-    margin: 0 0 24px 0;
+    margin: 0 0 25px 0;
+    height: 25px;
 `;
 
 export const List = styled.ul`
     padding: 0;
     margin: 0;
     list-style: none;
+    height: 140px;
+    overflow: hidden;
+    transition: .3s;
+
+    ${({ visible }) => visible && css`
+        ${({ elements }) => elements && css`
+            height: ${elements * 35}px;
+        `}
+    `}
 `;
 
 export const Item = styled.li`
-    margin: 0 0 12px 0;
+    height: 25px;
+    margin: 0 0 10px 0;
+    overflow: hidden;
 `;
 
 export const Link = styled.a`
     text-decoration: none;
     color: ${({ theme }) => theme.colors.darkGray};
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    white-space: pre-wrap;
     transition: .3s;
-    line-height: 1.45;
+    line-height: 25px;
 
     &:hover {
         color: ${({ theme }) => theme.colors.black};
