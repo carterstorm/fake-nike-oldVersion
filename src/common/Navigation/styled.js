3b302146b10eb51e11aa61fad4425bc431077558
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-07-25 20:43:34 +0200

    modified styles in Navigation

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -2,62 +2,60 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-    display: flex;
-    justify-content: space-between;
+    display: grid;
+    grid-template-columns: 300px 1fr 300px;
+    justify-content: space-around;
     align-items: center;
-    padding: 20px 0;
+    height: 65px;
 `;
 
 export const Logo = styled.h1`
-    margin: 0;
-    padding: 0 0 0 30px;
-    font-size: 30px;
-    font-style: italic;
-    cursor: pointer;
+    margin: 0 0 0 20px;
 `;
+
 
 export const List = styled.ul`
     display: flex;
+    justify-content: center;
     margin: 0;
-    margin-left: 150px;
     padding: 0;
     list-style: none;
+    width: 100%;
 `;
 
 export const Item = styled.li`
+    width: 100px;
     display: flex;
     justify-content: center;
-    min-width: 100px;
-    border-right: ${({ last }) => last ? "" : "1px solid black"};
 `;
 
 export const StyledNavLink = styled(NavLink)`
-    cursor: pointer;
     text-decoration: none;
     color: ${({ theme }) => theme.colors.black};
+    font-weight: 400;
 `;
 
 export const NavigationSection = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
-    margin: 0 30px 0 0;
+    justify-content: space-evenly;
 `;
 
 export const Client = styled.div`
-    margin-right: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `;
 
 export const UserOption = styled.a`
-    cursor: pointer;
-    margin: 0 10px;
+    margin: 0 5px;
 `;
 
 export const Languages = styled.div`
 `;
 
 export const LanguageButton = styled.button`
-    padding: 0 5px;
+    padding: 0 10px;
     background-color: transparent;
     border: none;
     border-right: ${({ last }) => last ? "" : "1px solid black"};
